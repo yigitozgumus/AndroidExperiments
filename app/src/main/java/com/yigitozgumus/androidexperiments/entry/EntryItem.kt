435a9ecaadbd6a9f72bdc7db3d583c5a6d80b653
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-02-13 13:19:19 +0300

    update Entry item data model to support inferred equals method

--- app/src/main/java/com/yigitozgumus/androidexperiments/entry/EntryItem.kt
+++ app/src/main/java/com/yigitozgumus/androidexperiments/entry/EntryItem.kt
@@ -3,10 +3,14 @@ package com.yigitozgumus.androidexperiments.entry
 import com.yigitozgumus.androidexperiments.demos.ParentChildFragmentDemoActivity
 import kotlin.reflect.KClass
 
-open class EntryItem(val title: String, val description: String, val entryPoint: KClass<out Any>)
-
-class ParentChildFragmentDemoItem : EntryItem(
-    title = "Parent Child Fragment Demo",
-    description = "Activity with parent and child activity showcasing the communication methods between components",
-    entryPoint = ParentChildFragmentDemoActivity::class
+sealed class EntryItem(
+    open val title: String,
+    open val description: String,
+    open val entryPoint: KClass<out Any>
 )
+
+data class ParentChildFragmentDemoItem(
+    override val title: String = "Parent Child Fragment Demo",
+    override val description: String = "Activity with parent and child activity showcasing the communication methods between components",
+    override val entryPoint: KClass<out Any> = ParentChildFragmentDemoActivity::class
+) : EntryItem(title, description, entryPoint)
